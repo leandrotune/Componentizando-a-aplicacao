36f660e42535cf225a48766d4c454ead07055ab4
Leandro Antunes <lele.tune@gmail.com>
2021-12-10 22:10:20 -0300

    Desafio componentizando aplicacao Concluido

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
+import { useContext, useEffect, useState } from "react";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss'
+import { MoviesContext } from "../hooks/GenreSelected";
+import { api } from "../services/api";
+
 export function SideBar() {
-  // Complete aqui
+  const { selectedGenreId, handleClickButton, genres } = useContext(MoviesContext)
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+    </div>
+  )
 }
